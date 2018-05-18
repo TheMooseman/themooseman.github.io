8d5d257c8f94197219012c3a0991f093382de8ae
themooseman <skylermoosman@gmail.com>
2018-05-18 08:15:15 -0700

    paddle moves the ball

--- js/breakout.js
+++ js/breakout.js
@@ -5,6 +5,46 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e)
+{
+	if(e.keyCode == 39)
+	{
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37)
+	{
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e)
+{
+	if(e.keyCode == 39)
+	{
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37)
+	{
+		leftPressed = false;
+	}
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -17,7 +57,8 @@ function drawBall() {
 function draw()
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
+	drawPaddle();
+	drawBall();
     
 	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius)
 	{
@@ -28,6 +69,15 @@ function draw()
 	{
 		dy = -dy;
 	}
+
+	if(rightPressed && paddleX < canvas.width-paddleWidth)
+	{
+        paddleX += 7;
+    }
+	else if(leftPressed && paddleX > 0)
+	{
+        paddleX -= 7;
+    }
 
 	x += dx;
 	y += dy;
