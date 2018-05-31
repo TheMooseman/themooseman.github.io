eccb0b88ec25a95e17b86715cca1022c7819ffb4
themooseman <skylermoosman@gmail.com>
2018-05-30 19:47:05 -0700

    text reading fill

--- js/textEditor.js
+++ js/textEditor.js
@@ -1,6 +1,11 @@
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
 
+c.font = "30px Comic Sans MS";
+c.fillStyle = "red";
+c.textAlign = "center";
+
+var text = 0;
 
 var file = document.getElementById("yourFile");
 var reader = new FileReader();
@@ -11,3 +16,5 @@ reader.onload = function(e)
 }
 
 reader.readAsText(file);
+
+c.fillText(text, canvas.width/2, canvas.height/2);
