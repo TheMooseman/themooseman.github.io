b3ec7c0a40bce1e48607c79c3a18aacb9c45d1be
themooseman <skylermoosman@gmail.com>
2018-05-30 19:48:11 -0700

    fill text try 2

--- js/textEditor.js
+++ js/textEditor.js
@@ -5,16 +5,14 @@ c.font = "30px Comic Sans MS";
 c.fillStyle = "red";
 c.textAlign = "center";
 
-var text = 0;
-
 var file = document.getElementById("yourFile");
 var reader = new FileReader();
 
 reader.onload = function(e)
 {
 	var text = reader.result;
+	c.fillText(text, canvas.width/2, canvas.height/2);
 }
 
 reader.readAsText(file);
 
-c.fillText(text, canvas.width/2, canvas.height/2);
