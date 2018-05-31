2864f036f767d4c8eafb61bdacf79503498b0513
themooseman <skylermoosman@gmail.com>
2018-05-30 20:04:35 -0700

    another

--- js/textEditor.js
+++ js/textEditor.js
@@ -5,50 +5,18 @@ c.font = "30px Comic Sans MS";
 c.fillStyle = "red";
 c.textAlign = "center";
 
-/*
 var file = document.getElementById("yourFile");
 var reader = new FileReader();
+var txt = "";
 
-function onChange(event)
+if ('files' in file)
 {
-	reader.onload = function(e)
+	if(file.files.length == 0)
 	{
-		console.log("ran script");
-		var text = reader.result;
-		c.fillText(text, canvas.width/2, canvas.height/2);
+		txt = "select file";
 	}
-	reader.readAsText(file);
-}*/
-
-var fileInput = $('#files');
-var uploadButton = $('#upload');
-
-uploadButton.on('click', function() {
-    if (!window.FileReader) {
-        alert('Your browser is not supported');
-        return false;
-    }
-    var input = fileInput.get(0);
-
-    // Create a reader object
-    var reader = new FileReader();
-    if (input.files.length) {
-        var textFile = input.files[0];
-        // Read the file
-        reader.readAsText(textFile);
-        // When it's loaded, process it
-        $(reader).on('load', processFile);
-    } else {
-        alert('Please upload a file before continuing')
-    } 
-});
-
-function processFile(e) {
-    var file = e.target.result,
-        results;
-    if (file && file.length) {
-        results = file.split("\n");
-        $('#name').val(results[0]);
-        $('#age').val(results[1]);
-    }
+	else
+	{
+		txt = file;
+	}
 }
