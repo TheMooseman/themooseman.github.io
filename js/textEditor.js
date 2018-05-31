a20d2844905c5d2e0e40bc01e381751a6d821bf4
themooseman <skylermoosman@gmail.com>
2018-05-30 19:40:49 -0700

    file reading

--- js/textEditor.js
+++ js/textEditor.js
@@ -2,3 +2,12 @@ var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
 
 
+var file = document.getElementById("yourFile");
+var reader = new FileReader();
+
+reader.onload = function(e)
+{
+	var text = reader.result;
+}
+
+reader.readAsText(file);
