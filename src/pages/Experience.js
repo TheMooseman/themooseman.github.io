c6a788b482d573432395d97090fef4605ecd3c83
skylermoosman@gmail.com <skylermoosman@gmail.com>
2022-08-06 15:11:54 -0700

    pdf viewer for my resume

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/Home.css';
 import { motion } from 'framer-motion';
+import {Document, Page, pdfjs} from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Experience() {
   return (
@@ -12,6 +14,15 @@ function Experience() {
       <div className="skills" id="skills">
         <h1> Skills</h1>
         <span>JavaScript, C++, C#, TypeScript, Web, Games, Audio</span>
+        <a href='https://drive.google.com/file/d/1U5Lf-A6zvvcJ-X-o4OikQShZnAuqq483/view?usp=sharing'>
+        <Document
+          file={"https://drive.google.com/file/d/1U5Lf-A6zvvcJ-X-o4OikQShZnAuqq483/view?usp=sharing"}
+          onLoadError={console.error}
+          style={{ width: '100vw', height: 'auto'}}
+        >
+          <Page pageIndex={0} />
+        </Document>
+        </a>
       </div>
     </motion.div>
   );
